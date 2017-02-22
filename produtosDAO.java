4c64c01f80e264d529542d335117371b297109b3
Negreyscow <leeo.apc@gmail.com>
2017-02-22 11:05:44 -0300

    ssa
    total com o banco

---
+++ produtosDAO.java
@@ -0,0 +1,175 @@
+package app.model.dao;
+
+import app.model.database.ConnectionFactory;
+import app.model.domain.Produtos;
+import javafx.fxml.FXML;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Aristarco on 06/01/17.
+ */
+
+public class  produtosDAO { //esta classe realizar as operações básicas de manipulação de banco de dados para os produtos
+
+
+    private Connection connection;
+
+    public produtosDAO() {
+        connection = new ConnectionFactory().getConnection();
+    }
+
+    public void cadastrar(Produtos produto) throws SQLException {
+
+        String sql = "insert into produtos (nome, preco, quantidade, idproduto) values (?, ?, ?,?)";
+
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, produto.getNome());
+            statement.setDouble(2, produto.getPreco());
+            statement.setInt(3, produto.getQuantidade());
+            statement.setString(4, produto.getCategoria());
+
+
+            statement.execute();
+            statement.close();
+
+    }
+
+
+
+    public void cadastrarTotal(double total) throws SQLException {
+
+        String sql = "insert into tabelatotal (valor) values (?)";
+
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+
+
+        statement.execute();
+        statement.close();
+
+    }
+
+    public void alterar(Produtos produtos) throws SQLException{
+
+        String sql = "update produtos set nome=?, preco=?, quantidade=?, idproduto=? where cdproduto=?";
+
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1, produtos.getNome());
+            statement.setDouble(2, produtos.getPreco());
+            statement.setInt(3, produtos.getQuantidade());
+            statement.setString(4, produtos.getCategoria());
+            statement.setInt(5, produtos.getCdproduto());
+
+            statement.execute();
+            statement.close();
+
+
+    }
+
+    public void deletar(Integer idProduto){
+
+        String sql = "delete from produtos where cdproduto=?";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, idProduto);
+
+            statement.execute();
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<Produtos> consultar(String nomeProduto){
+
+        List<Produtos> produtos = new ArrayList<>();
+
+        String sql = "select * from produtos where nome like '%" + nomeProduto + "%'";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            while(resultSet.next()){
+
+                Produtos produto = new Produtos();
+
+                produto.setCdproduto(resultSet.getInt("cdproduto"));
+                produto.setNome(resultSet.getString("nome"));
+                produto.setQuantidade(resultSet.getInt("quantidade"));
+                produto.setPreco(resultSet.getDouble("preco"));
+                produto.setCategoria(resultSet.getString("idproduto"));
+
+                produtos.add(produto);
+
+            }
+
+            resultSet.close();
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        return produtos;
+    }
+
+    public List<Produtos> consultar2(String nomeProduto){
+
+        List<Produtos> produtos = new ArrayList<>();
+
+        String sql = "select * from produtos where idproduto like '%" + nomeProduto + "%'";
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+
+            while(resultSet.next()){
+
+                Produtos produto = new Produtos();
+
+               // produto.setCdproduto(resultSet.getInt("cdproduto"));
+                produto.setNome(resultSet.getString("nome"));
+                produto.setQuantidade(resultSet.getInt("quantidade"));
+                produto.setPreco(resultSet.getDouble("preco"));
+                produto.setCategoria(resultSet.getString("idproduto"));
+
+                produtos.add(produto);
+
+            }
+
+            resultSet.close();
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        return produtos;
+    }
+
+    ///aditional
+
+
+
+
+
+}
---
+++ vendasController.java
@@ -0,0 +1,347 @@
+package app.controllers;
+
+import app.Main;
+import app.model.dao.produtosDAO;
+import app.model.dao.vendasDAO;
+import app.model.domain.Produtos;
+import javafx.beans.InvalidationListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
+import javafx.util.Callback;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.*;
+
+import static app.Main.mainLayout;
+
+/**
+ * Created by Leonardo on 19/02/2017.
+ */
+
+public class vendasController implements Initializable {
+
+    private Main main;
+
+    private List<String> listAux = new ArrayList<>();
+
+    @FXML
+    public ListView<String> listView;
+
+    private vendasDAO daoV;
+
+    public List<String> listListView;
+
+    public ObservableList<String> observableListView;
+
+    private produtosDAO dao;
+
+    private Produtos produtoSelecionado;
+
+    @FXML
+    private ComboBox<String> comboBoxBusca;
+
+    private List<String>  listComboBox = new ArrayList<>();
+    private ObservableList<String> observableListComboBox;
+
+    //private List<Produtos> listCarrinho;
+    //private ObservableList<Produtos> observableListCarrinho;
+
+    @FXML
+    private TableColumn<Produtos, String> idColumn;
+
+    @FXML
+    private TableColumn<Produtos, String> produtoColumn;
+
+    @FXML
+    private TableColumn<Produtos, String> categoriaColumn;
+
+    @FXML
+    private TableColumn<Produtos, Integer> qtdColumn;
+
+    @FXML
+    private TableColumn<Produtos, BigDecimal> valorColumn;
+
+    @FXML
+    private TextField fieldBuscar;
+
+    @FXML
+    private TableView<Produtos> tableVendasItems;
+
+
+    //table view do carrinho de compras
+    @FXML
+    private TableView<Produtos> tableCarrinho;
+
+    @FXML
+    private TableColumn<Produtos, String> columnProdutoCar;
+
+    @FXML
+    private TableColumn<Produtos, Integer> columnQtdCar;
+
+    @FXML
+    private TableColumn<Produtos, BigDecimal> columnValorCar;
+
+    private List<Produtos> listCarrinho = new ArrayList();
+
+    public ObservableList<Produtos> observableListCarrinho;
+
+    @FXML
+    private Label fieldTotal;
+
+    public double totalCompra;
+
+    private static ArrayList<String> words;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        carregarComboBox();
+
+        dao = new produtosDAO();
+        daoV = new vendasDAO();
+        goBuscar();
+        listListView = new ArrayList<String>();
+
+        idColumn.setCellValueFactory(new PropertyValueFactory<Produtos, String>("categoria"));
+        produtoColumn.setCellValueFactory(new PropertyValueFactory<Produtos, String>("nome"));
+        valorColumn.setCellValueFactory(new PropertyValueFactory<Produtos, BigDecimal>("preco"));
+        qtdColumn.setCellValueFactory(new PropertyValueFactory<Produtos, Integer>("quantidade"));
+
+    }
+
+
+    //combo box para a categoria da busca
+    public void carregarComboBox(){
+
+        listComboBox.add("Nome Produto");
+        listComboBox.add("Codigo Produto");
+
+        observableListComboBox = FXCollections.observableArrayList(listComboBox);
+        //não é possivel popular com listClientes
+        comboBoxBusca.setItems(observableListComboBox);
+
+    }
+
+    public double getTotalCompra(){
+        return this.totalCompra;
+    }
+
+
+    public void carregarListView(){
+
+
+        produtoSelecionado = tableVendasItems.getSelectionModel().getSelectedItem();
+        if (produtoSelecionado == null)
+            exibirDialogoInformacao("Precisa selecionar um item!");
+        else {
+            if (!listListView.contains(produtoSelecionado.getNome()))
+                listListView.add(produtoSelecionado.getNome() + "           -          Valor R$: " + produtoSelecionado.getPreco());
+            else
+                System.out.println("works!");
+
+
+            observableListView = FXCollections.observableArrayList(listListView);
+            listView.setItems(observableListView);
+        }
+        carregarTableViewCarrinho();
+    }
+
+    public List<String> getList(){
+        return listListView;
+    }
+
+    @FXML
+    void removerList(){
+        String algo = listView.getSelectionModel().getSelectedItem();
+        String aux;
+        int contador = algo.length();
+        for(int i = 0;i<contador;i++){
+            if (algo.substring(i,i+1).equals(":")){
+                int posicao = i+1;
+                aux=algo.substring(posicao,algo.length());
+                double valor=Double.parseDouble(aux);
+
+                totalCompra -=valor;
+                totalCompra = totalCompra*100;
+                totalCompra = Math.round(totalCompra);
+                totalCompra = totalCompra/100;
+                fieldTotal.setText(Double.toString(totalCompra));
+            }}
+
+        listListView.remove(algo);
+        listView.getItems().remove(algo);
+        listAux.remove(algo);
+    }
+
+    public List getListAux(){
+        return listAux;
+    }
+
+    public void carregarTableViewCarrinho(){
+
+        produtoSelecionado = tableVendasItems.getSelectionModel().getSelectedItem();
+
+        //columnProdutoCar.setCellValueFactory(new PropertyValueFactory<Produtos, String>("nome"));
+        //columnQtdCar.setCellValueFactory(new PropertyValueFactory<Produtos, Integer>("quantidade"));
+        //columnValorCar.setCellValueFactory(new PropertyValueFactory<Produtos, BigDecimal>("preco"));
+
+        listCarrinho.add(produtoSelecionado);
+        //comentario
+        //observableListCarrinho = FXCollections.observableArrayList(listCarrinho);
+        //tableCarrinho.setItems(observableListCarrinho);
+
+        totalCompra += produtoSelecionado.getPreco();
+        fieldTotal.setText(Double.toString(totalCompra));
+
+
+    }
+
+    public ObservableList<Produtos> getTableView(){
+        return observableListCarrinho;
+    }
+
+    @FXML
+    public void removerTableViewCarrinho(){
+        Produtos produto = tableCarrinho.getSelectionModel().getSelectedItem();
+        tableCarrinho.getItems().remove(produto);
+
+    }
+
+
+    @FXML
+    void goCarrinho(){
+
+       // carregarTableViewCarrinho();
+        carregarListView();
+        //pega o item selecionado na tabela
+
+        //produtoSelecionado = tableVendasItems.getSelectionModel().getSelectedItem();
+        /*
+        observableListCarrinho = FXCollections.observableArrayList(produtoSelecionado);
+        tableCarrinho.setItems(observableListCarrinho);
+        System.out.println(produtoSelecionado.getNome());*/
+
+       // String name = produtoSelecionado.getNome();
+        //Integer quantidade = produtoSelecionado.getQuantidade();
+       // BigDecimal preco = produtoSelecionado.getPreco();
+
+       // mylist.add(new myObject(name,quantidade,preco));
+
+        /*
+        ListView<myObject> listView = new ListView<>();
+        ObservableList<myObject> myObjectObservableList = FXCollections.observableList(mylist);
+        listView.setCellFactory(new Callback<ListView<myObject>, ListCell<myObject>>() {
+            @Override
+            public ListCell<myObject> call(ListView<myObject> param) {
+                ListCell<myObject> cell = new ListCell<myObject>(){
+                    @Override
+                    protected void updateItem(myObject t,boolean bln){
+                        super.updateItem(t, bln);
+                        if (t != null){
+                            setText(t.getNomee() + ":" + t.getQtd() + ":" + t.getManey());
+                        }
+                    }
+                };
+                return cell;
+            }
+        }); */
+
+
+    }
+
+    @FXML
+    void goNextView() throws IOException, SQLException {
+
+        if (listView.getItems().isEmpty()){
+           exibirDialogoErro("Carrinho vazio!");
+        }
+        else{
+
+            exibirDialogoConfirmação("Confirmar a seleção de items");
+
+            dao.cadastrarTotal(totalCompra);
+
+            main.showSecondViewVenda();
+        }
+
+
+
+
+    }
+
+
+    @FXML
+    void goBuscar() {
+
+        try {
+
+            List<Produtos> resultado;
+
+            if (comboBoxBusca.getValue() == "Codigo Produto"){
+                resultado = dao.consultar2(fieldBuscar.getText());
+            }
+            else {
+                resultado = dao.consultar(fieldBuscar.getText());
+            }
+
+            if(resultado.isEmpty()){
+                exibirDialogoInformacao("Nenhum Resultado foi encontrado");
+            } else {
+
+                tableVendasItems.setItems(FXCollections.observableList(resultado));
+            }
+
+        } catch (Exception e){
+            exibirDialogoErro("Falha ao realizar a consulta");
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //alerts
+
+    private void exibirDialogoInformacao(String informacao){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Informação");
+        alert.setHeaderText(null);
+        alert.setContentText(informacao);
+
+        alert.showAndWait();
+    }
+
+    private void exibirDialogoErro(String erro){
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Erro");
+        alert.setHeaderText(null);
+        alert.setContentText(erro);
+
+        alert.showAndWait();
+    }
+
+    private boolean exibirDialogoConfirmação(String confirmacao){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmação");
+        alert.setHeaderText(null);
+        alert.setContentText(confirmacao);
+
+        Optional<ButtonType> opcao = alert.showAndWait();
+        if (opcao.get() == ButtonType.OK) return true;
+
+        return false;
+
+    }
+
+
+}
