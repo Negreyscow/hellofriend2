9e2103b8cb66432a038a67b9a9484160c97f2c02
Bruno <brunohenrique.laier28@gmail.com>
2017-02-23 01:08:35 -0300

    Nome Hellofriend nas telas

--- src/app/Main.java
+++ src/app/Main.java
@@ -27,7 +27,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
 		this.primaryStage = primaryStage;
-		this.primaryStage.setTitle("MaisVoce");
+		this.primaryStage.setTitle("HelloFriend");
 
 
         showLogin();
